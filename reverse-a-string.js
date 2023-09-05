fa27967fdf431dc51ea18e1363717665c4027b5a
Jesus Petit <96022350+jesuspetit11@users.noreply.github.com>
2023-09-05 06:13:18 -0400

    added another way to reverse a string

--- reverse-a-string.js
+++ reverse-a-string.js
@@ -4,3 +4,13 @@ function reverseString(str) {
 }
 
 reverseString("hello");
+
+//Another way
+
+function reverseString(str){
+    let newStr = "";
+for (let i = a.length -1; i >= 0; i--) {
+    newStr = str[i];
+    }
+    return newStr;
+}
